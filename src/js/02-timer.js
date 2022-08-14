ffca992c4a6a52adfdf5f256bb30134c8715022c
VladaBoiko <bermezha@gmail.com>
2022-08-14 20:04:52 +0300

    gsfgh

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,13 @@
+refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  clockFace: document.querySelector('.timer'),
+  dayValue: document.querySelector('[data-days]'),
+  hoursValue: document.querySelector('[data-hours]'),
+  minutesValue: document.querySelector('[data-minutes]'),
+  secundesValue: document.querySelector('[data-seconds]'),
+};
+
 const timer = {
   start() {
     const startTime = Date.now();
@@ -6,8 +16,8 @@ const timer = {
     setInterval(() => {
       const currentTime = Date.now();
       const deltaTime = currentTime - startTime;
-      const timeComponents = getTimeComponents(deltaTime);
-      console.log(timeComponents);
+      const { days, hours, mins, secs } = getTimeComponents(deltaTime);
+      console.log(`${days} :: ${hours} :: ${mins} :: ${secs}`);
 
       // console.log(new Date(deltaTime).getUTCHours());
     }, 1000);
@@ -18,10 +28,20 @@ function pad(value) {
   return String(value).padStart(2, '0');
 }
 function getTimeComponents(time) {
+  const days = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60 * 24))
+  );
   const hours = pad(
     Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
   );
   const mins = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60)));
   const secs = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / 1000));
-  return { hours, mins, secs };
+  return { days, hours, mins, secs };
 }
+function updateTimerFace({ days, hours, mins, secs }) {
+  refs.dayValue.textContent = `${days}`;
+  refs.hoursValue.textContent = `${hours}`;
+  refs.minutesValue.textContent = `${mins}`;
+  refs.secundesValue.textContent = `${secs}`;
+}
+updateTimerFace();
