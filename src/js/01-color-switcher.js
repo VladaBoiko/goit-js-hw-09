cd43f7081e43efc9b5c1522243058ede35f52ae4
VladaBoiko <bermezha@gmail.com>
2022-08-18 19:17:04 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,7 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
+let colorId = null;
 const refs = {
   body: document.querySelector('body'),
   startBtn: document.querySelector('[data-start]'),
